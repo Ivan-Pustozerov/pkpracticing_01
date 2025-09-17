1e0e7159d487a66a14facb321efc5968653ea64b
Ivan <pustozerov.ivan@mail.ru>
2025-09-17 22:41:33 +0400

    Branch test Ver 0.0.0 17/09/2025

--- src/main/java/ru/ssau/tk/pkdevs/practicing/HelloTest.java
+++ src/main/java/ru/ssau/tk/pkdevs/practicing/HelloTest.java
@@ -3,5 +3,6 @@ package ru.ssau.tk.pkdevs.practicing;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello, PKDevs!");
+        System.out.println("Test");
     }
 }
