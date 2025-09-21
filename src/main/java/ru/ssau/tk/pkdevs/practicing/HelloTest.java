7b3da8972a3cf69a7f91aa3792498f891c233e17
Pustozerov <pustozerov.ivan@mail.ru>
2025-09-21 21:07:06 +0300

    Deleting trash files version 0.2.1.1 21/09/2025

--- src/main/java/ru/ssau/tk/pkdevs/practicing/HelloTest.java
+++
@@ -1,8 +0,0 @@
-package ru.ssau.tk.pkdevs.practicing;
-
-public class HelloTest {
-    public static void main(String[] args) {
-        System.out.println("Hello, PKDevs!");
-        System.out.println("Test");
-    }
-}
