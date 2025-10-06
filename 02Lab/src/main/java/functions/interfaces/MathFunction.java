64b58925c63996e0a5412985e5be6597e022ab49
Dr Woid <llparanormal.foxll@gmail.com>
2025-10-06 18:11:43 -0100

    CompositeFunction with tests(+FIX MathFunction) Version 0.2.1.5

--- 02Lab/src/main/java/functions/interfaces/MathFunction.java
+++ 02Lab/src/main/java/functions/interfaces/MathFunction.java
@@ -5,6 +5,6 @@ import functions.classes.CompositeFunction;
 public interface MathFunction {
     double apply(Object x);//any method will be auto public in implemented class
     default CompositeFunction andThen(MathFunction afterFunction){
-        return new CompositeFunction(this,afterFunction);
+        return new CompositeFunction(afterFunction,this);
     }
 }
