99109882df85f505ddb76133541a3d60cf031171
Ivan <pustozerov.ivan@mail.ru>
2025-11-03 14:42:17 +0400

    Factory without tests
    Ver: 0.3.2.1
    03/11/2025

---
+++ 03Lab/src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -0,0 +1,12 @@
+package functions.factory;
+
+import functions.classes.LinkedListTabulatedFunction;
+import functions.interfaces.TabulatedFunction;
+
+public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory{
+
+    @Override
+    public TabulatedFunction create(double[] xValues, double[] yValues) {
+        return new LinkedListTabulatedFunction(xValues, yValues);
+    }
+}
