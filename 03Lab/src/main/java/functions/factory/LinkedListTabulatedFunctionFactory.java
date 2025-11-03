e8bf31ad4dc2228b3dcf11c6aaf278bb12cff09f
Ivan <pustozerov.ivan@mail.ru>
2025-11-03 20:49:37 +0400

    createUnmodifiable in Factory + tests
    Ver: 0.3.2.8
    03/11/2025

--- 03Lab/src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ 03Lab/src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -4,7 +4,6 @@ import functions.classes.LinkedListTabulatedFunction;
 import functions.interfaces.TabulatedFunction;
 
 public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory{
-
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new LinkedListTabulatedFunction(xValues, yValues);
